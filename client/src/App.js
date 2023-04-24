9d7c4ab23e5cc9b6b29f1619cb5f9565bc40182d
Antonio Furtado <antoniordf@hotmail.com>
2023-04-24 14:21:04 +0100

    Updated App.js with an initial boilerplate code to display front end depending on user holding NFT. Also updated truffle-config, installed HDWalletProvider and dotenv

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { initWeb3, initContract, userHasNFT } from '../utils/web3'; // Import the utility functions
+import DApp from '../contracts/DApp.json'; // Import the compiled contract JSON
+
 
 function App() {
+  const [hasAnyNFT, setHasAnyNFT] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const dAppContractAddress = /* Your deployed DApp contract address */;
+
+  useEffect(() => {
+    const setup = async () => {
+      await initWeb3();
+      await initContract(DApp.abi, dAppContractAddress);
+
+      const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+      const userAddress = accounts[0];
+
+      const result = await userHasNFT(userAddress);
+      setHasAnyNFT(result);
+      setLoading(false);
+    };
+
+    setup();
+  }, []);
+
+  // Render the content conditionally based on the hasAnyNFT state
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? (
+        <div>Loading...</div>
+      ) : hasAnyNFT ? (
+        // Display the main content of your app
+        <div>Welcome, NFT holder! You can now access the content.</div>
+      ) : (
+        // Display a message indicating the user doesn't have any NFT from the collection
+        <div>Sorry, you do not have any NFT from the specified collection. Access denied.</div>
+      )}
     </div>
   );
 }
