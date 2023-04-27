ec1f351a44f2fac32ab4474cbaa3106f2aff3861
Antonio Furtado <antoniordf@hotmail.com>
2023-04-27 23:11:48 +0100

    Finished creating routes for all CRUD operations. The RESTful API is complete.

--- server.js
+++ server.js
@@ -32,11 +32,47 @@ app.get("/todos", async (req, res) => {
   }
 });
 
-// get a todo
+// get a specific todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todos WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 // update a todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todos SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+    res.json("Todo was updated");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 // delete a todo
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query(
+      "DELETE FROM todos WHERE todo_id = $1",
+      [id]
+    );
+    res.json(`Todo ${id} has been deleted`);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 // Start the server
 const PORT = process.env.PORT || 6000;
